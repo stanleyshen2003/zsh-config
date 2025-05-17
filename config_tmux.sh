57a86415781aa965abfa9b55c40d9fa6c93f68d3
stanleyshen2003 <stanley.shen2003@gmail.com>
2025-05-18 01:13:52 +0800

    Update config_tmux.sh

--- config_tmux.sh
+++ config_tmux.sh
@@ -1,8 +1,11 @@
 sudo apt install tmux -y
-echo "set-option -g default-shell $(which zsh)" >> ~/.tmux.conf
-echo "set -g default-terminal \"xterm-256color\"" >> ~/.tmux.conf
-echo "set-option -g mouse on" >> ~/.tmux.conf
-echo "set-option -g status-style \"bg=#76B5C5,fg=#000000\"" >> ~/.tmux.conf
+
+cat <<EOF >> ~/.tmux.conf
+set-option -g default-shell $(which zsh)
+set -g default-terminal "xterm-256color"
+set-option -g mouse on
+set-option -g status-style "bg=#76B5C5,fg=#000000"
+EOF
 
 tmux new-session -d -s temp
 tmux source-file ~/.tmux.conf
